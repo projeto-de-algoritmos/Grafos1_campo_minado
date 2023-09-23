994d1a65d449001668dc0371aa97be2cd93cfc45
Charles Serafim Morais <75335915+charles-serafim@users.noreply.github.com>
2023-09-23 19:36:43 -0300

    scripts, assets, flood fill com DFS e pausas para visualização

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    [SerializeField]
+    private bool mine; // Is this element a mine?
+
+    [SerializeField]
+    private Sprite[] emptyTextures; // List of empty textures i.e. empty, 1, 2, 3, 4, 5, 6, 7, 8
+
+    [SerializeField]
+    private Sprite mineTexture; // Mine texture
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mine = Random.value < 0.15; // 15% chance of being a mine
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+
+        GameController.elements[x, y] = this; // Register in grid
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsMine()
+    {
+        return this.mine;
+    }
+
+    public void LoadTexture(int adjacentCount)
+    {
+
+        if (mine)
+        {
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+        }
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (mine)
+        {
+            // tela de game over
+            this.LoadTexture(0);
+            print("Game Over!!!");
+            GameController.UncoverMines();
+        }
+        else
+        {
+            // lógica proximidade mina
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+
+            int adjacentMines = GameController.AdjacentMines(x, y);
+
+            this.LoadTexture(adjacentMines);
+            
+            bool[,] visited = new bool[GameController.gridWidth, GameController.gridHeight];
+
+            StartCoroutine(GameController.DFSFFuncover(x, y, visited));
+            
+            // GameController.DFSFFuncover(x, y, new bool[GameController.gridWidth, GameController.gridHeight]);
+
+            if(GameController.IsFinished())
+            {
+                print("You Win!!!");
+            }
+        }
+    }
+
+    public bool IsCovered()
+    {
+        if(GetComponent<SpriteRenderer>().sprite.texture.name == "default")
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] // SerializeField makes private variables visible in the inspector
+    private GameObject block; // Prefab of the block
+
+    public static int gridWidth = 10;
+    public static int gridHeight = 13;
+
+    public static Element[,] elements = new Element[gridWidth, gridHeight]; // Array of elements
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.CreateField();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Create the mine field
+    private void CreateField()
+    {
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                Instantiate(block, new Vector3(i, j, 0f), Quaternion.identity);
+            }
+        }
+    }
+
+    public static void UncoverMines()
+    {
+        foreach (Element item in elements)
+        {
+            if(item.IsMine())
+            {
+                item.LoadTexture(0);
+            }
+        }
+    }
+
+    // Is the element at the given position a mine?
+    public static bool MineAt(int x, int y)
+    {
+        // Coordinates in range? Then check for mine.
+        if (x >= 0 && y >= 0 && x < gridWidth && y < gridHeight)
+        {
+            return elements[x, y].IsMine();
+        }
+        return false;
+    }
+
+    public static int AdjacentMines(int x, int y)
+    {
+        int count = 0;
+        for(int i = x - 1; i <= x + 1; i++)
+            for(int j = y - 1; j <= y + 1; j++)
+                if(MineAt(i, j)) count++;
+        return count;
+    }
+
+    public static IEnumerator DFSFFuncover(int x, int y, bool[,] visited)
+    {
+        // Coordinates in range?
+        if (x >= 0 && y >= 0 && x < gridWidth && y < gridHeight)
+        {
+            if (visited[x, y]) yield break;
+
+            visited[x, y] = true;
+
+            yield return new WaitForSeconds(0.2f);
+
+            elements[x, y].LoadTexture(AdjacentMines(x, y));
+
+            if (AdjacentMines(x, y) > 0) yield break;
+
+            yield return DFSFFuncover(x - 1, y, visited);
+            yield return DFSFFuncover(x - 1, y + 1, visited);
+            yield return DFSFFuncover(x, y + 1, visited);
+            yield return DFSFFuncover(x + 1, y + 1, visited);
+            yield return DFSFFuncover(x + 1, y, visited);
+            yield return DFSFFuncover(x + 1, y - 1, visited);
+            yield return DFSFFuncover(x, y - 1, visited);
+            yield return DFSFFuncover(x - 1, y - 1, visited);
+
+        }
+    }
+
+    public static bool IsFinished()
+    {
+        // Try to find a covered element that is no mine
+        foreach (Element item in elements)
+            if(item.IsCovered() && !item.IsMine())
+                return false;
+                
+        // There are none => all are mines => game won.
+        return true;
+    }
+}
